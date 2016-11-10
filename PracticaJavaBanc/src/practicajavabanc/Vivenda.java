18e610fc4227e2a08b3faf0a48de0888d7606e99
Carlos-Rodero <carlos.rodero.garcia@gmail.com>
2016-11-10 11:07:26 +0100

    Add files via upload

--- PracticaJavaBanc/src/practicajavabanc/Vivenda.java
+++ PracticaJavaBanc/src/practicajavabanc/Vivenda.java
@@ -1,22 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Carlos Rodero, Esteve Cabrera, Mario Recamales
  */
-package practicajavabanc;
 
 import java.util.Date;
 
-/**
- *
- * @author Sandra
- */
+
 public class Vivenda extends CompteBancari {
+
     protected int tipusInteres;
     protected Date dataCaducitat;
-    public Vivenda(String iban, double saldo, Client propietari) {
+
+    public Vivenda(String iban, double saldo, Client propietari, Date dataCaducitat) {
         super(iban, saldo, propietari);
+        this.dataCaducitat = dataCaducitat;
     }
-    
-    
+
+    public int getTipusInteres() {
+        return tipusInteres;
+    }
+
+    public void setTipusInteres(int tipusInteres) {
+        this.tipusInteres = tipusInteres;
+    }
+
+    public Date getDataCaducitat() {
+        return dataCaducitat;
+    }
+
+    @Override
+    public void ingressarDiners(double importe){
+        saldo = saldo+importe+(importe*tipusInteres/100);
+    }
 }
