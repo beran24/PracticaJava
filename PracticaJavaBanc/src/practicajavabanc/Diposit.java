d1db8f3d974b827fb8b8a6b0da10340db0cad716
Carlos-Rodero <carlos.rodero.garcia@gmail.com>
2016-11-14 13:23:06 +0100

    Add files via upload

--- PracticaJavaBanc/src/practicajavabanc/Diposit.java
+++ PracticaJavaBanc/src/practicajavabanc/Diposit.java
@@ -1,36 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practicajavabanc;
+//package practicajavabanc;
 
 import java.util.Date;
-
 /**
- *
- * @author Sandra
+ * @author Carlos Rodero, Esteve Cabrera, Mario Recamales
  */
 public class Diposit extends CompteBancari {
+    /**
+     * Classe Diposit, hereta de la classe CompteBancari.
+     * Atributs de la classe PlaPensions:
+     * protected int tipusInteres;
+     * public double minDiners;
+     * protected Date dataAvui;
+     * protected Date dataUltimaVerificacio;
+     */
+    //Atributs
     protected int tipusInteres=1;
     public double minDiners=10000.00;
     protected Date dataAvui;
     protected Date dataUltimaVerificacio;
+    //Constructor
 
-    public double getMinDiners() {
-        return minDiners;
-    }
-    
+    /**
+     * Constructor de la classe Diposit
+     * @param iban
+     * @param saldo
+     * @param propietari
+     */
     public Diposit(String iban, double saldo, Client propietari) {
         super(iban, saldo, propietari);
         this.dataUltimaVerificacio=new Date();
     }
+
+    //Mètodes públics
+
+    /**
+     * Getter tipusInteres
+     * @return el tipus d'interés del compte Dipòsit
+     */
+    public int getTipusInteres() {
+        return tipusInteres;
+    }
+
+    /**
+     * Setter tipusInteres. Estableix el tipus d'interés del compte Dipòsit
+     * @param tipusInteres
+     */
+    public void setTipusInteres(int tipusInteres) {
+        this.tipusInteres = tipusInteres;
+    }
+    /**
+     * Getter MinDiners.
+     * @return el mínim de diners que acceptarà el compte Dipòsit
+     */
+    public double getMinDiners() {
+        return minDiners;
+    }
+
+    /**
+     * Mètode comprovarInteressos. Aquest mètode permet guardar la data en la que accedim, i d'aquesta manera
+     * ens permet calcular els interessos que s'han generat desde la darrera data en que hem accedit al mètode.
+     * Cada 60 segons (60.000 ms) augmenta per 1 unitat els interessos.
+     * Per exemple, si passen 120 segons augmenten *2 els interessos
+     * Per pantalla se'ns mostrarà aquesta informació i s'afegiran al saldo del compte Diposit
+     * @param dataAvui
+     */
     public void comprovarInteressos(Date dataAvui){
         this.dataAvui=new Date();
         saldo=saldo+((saldo*tipusInteres/100)*(dataAvui.getTime()-dataUltimaVerificacio.getTime())/60000);
         System.out.println("Els interesos desde la ultima verificació son: "+(saldo*tipusInteres/100)+(dataAvui.getTime()-dataUltimaVerificacio.getTime())/60000);
         this.dataUltimaVerificacio=new Date();
     }
+
+    /**
+     * Mètode treureDiners. Disminuïm el saldo del compte Dipòsit en la quantitat indicada.
+     * Si en treure diners el saldo queda negatiu no ens permet fer la operació.
+     * @param importe
+     * @return true si hem pogut treure diners (importe) en el compte Dipòsit. Sino, retorna false.
+     */
     public boolean treureDiners(double importe){
         if(saldo-importe>0){
             saldo=saldo-importe;
@@ -39,6 +85,4 @@ public class Diposit extends CompteBancari {
         else return false;
             
     }
-    
-    
 }
